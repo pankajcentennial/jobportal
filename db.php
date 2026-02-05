b8163847c9eeeb88b3b8838dad3929f79a3e382e
pankajcentennial <pankaj@centennialinfotech.com>
2026-02-05 14:42:30 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,26 +3,25 @@
 $databaseUrl = getenv("DATABASE_URL");
 
 if (!$databaseUrl) {
-    die("DATABASE_URL not set!");
+    die("DATABASE_URL is not set in Render Environment Variables!");
 }
 
 $dbparts = parse_url($databaseUrl);
 
-$host = $dbparts['dpg-d624k7sr85hc73dd8nig-a'];
-$port = $dbparts[5432];
-$user = $dbparts['jobportal_uxkc_user'];
-$pass = $dbparts['8d2VAR3Zg1z9J46VDB4z2tuesMBaorAN'];
+$host = $dbparts["host"];
+$port = $dbparts["port"];
+$user = $dbparts["user"];
+$pass = $dbparts["pass"];
 $dbname = ltrim($dbparts["path"], "/");
 
 try {
     $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require";
+
     $pdo = new PDO($dsn, $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 
-    // Connected
-    // echo "Connected Successfully";
-
+    echo "PostgreSQL Connected Successfully!";
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
