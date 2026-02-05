03200ea68337dd3a4e9e9b1595e469aa0e40eed6
pankajcentennial <pankaj@centennialinfotech.com>
2026-02-05 14:24:25 +0530

    Update db.php

--- db.php
+++ db.php
@@ -8,9 +8,9 @@ if (!$databaseUrl) {
 
 $dbparts = parse_url($databaseUrl);
 
-$host = $dbparts[dpg-d624k7sr85hc73dd8nig-a];
+$host = $dbparts['dpg-d624k7sr85hc73dd8nig-a'];
 $port = $dbparts[5432];
-$user = $dbparts[jobportal_uxkc_user];
+$user = $dbparts['jobportal_uxkc_user'];
 $pass = $dbparts['8d2VAR3Zg1z9J46VDB4z2tuesMBaorAN'];
 $dbname = ltrim($dbparts["path"], "/");
 
